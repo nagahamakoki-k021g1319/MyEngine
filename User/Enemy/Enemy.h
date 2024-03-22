3a1c05e7a78f53a2750579906301dcc6698be2ad
nagahama_kouki <k021g1319@g.neec.ac.jp>
2024-03-22 21:38:07 +0900

    03/22(金)Enemyの基底クラスのフラグと敵タイプの制作

--- User/Enemy/Enemy.h
+++ User/Enemy/Enemy.h
@@ -2,21 +2,27 @@
 #include"Model.h"
 #include"Object3d.h"
 
-struct EnemyState
-{
-	bool isDead_ = true;
-	int hp_ = 1;
-};
 
 class Enemy
 {
-//public:
-//	// 初期化
-//	virtual void Initialize(Model* Model) = 0;
-//	// 更新
-//	virtual void Update() = 0;
-//	// 描画
-//	virtual void Draw(ID3D12GraphicsCommandList* cmdList) = 0;
-//	// その他の敵の共通機能
+public:
+	//getter
+	//IsDead
+	bool GetIsDead() const {return isDead_;}
+	bool GetIsDelete() const {return isDelete_;}
+	bool GetIsHit() const {return isHit_;}
+	int GetHP() const {return hp_;};
+
+public:
+	//デスフラグ
+	//倒されたか
+	bool isDead_ = false;
+	//デスポーンしたか
+	bool isDelete_ = false;
+	//弾が当たったか
+	bool isHit_ = false;
+
+	//体力
+	int hp_;
 
 };
